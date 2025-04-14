3c68d577b8395fc2a0fe44c4fdb2ec51f768a9cc
Steven Reed <stevenreed122@gmail.com>
2025-04-13 23:40:19 -0500

    Testing member info

--- src/test/java/com/example/MemberTest.java
+++ src/test/java/com/example/MemberTest.java
@@ -25,7 +25,15 @@ public class MemberTest {
     @BeforeEach
     void setup() {
         library = mock(Library.class);
-        book = mock(Book.class);
+        // its just as easy form me to use a real book object
+        book = new Book(
+                "The Lion, The Witch, and The Wardrobe",
+                "CS Lewis",
+                1950,
+                "9780001831803",
+                "Fantasy",
+                library
+        );
         member = new Member("Reed, Steven", "sreed43@lsu.edu", 0);
     }
 
@@ -36,13 +44,23 @@ public class MemberTest {
         assertEquals("Reed, Steven", member.getName());
     }
 
-    // Testing adding a book to the member's borrowedBooksList
+    // Testing adding and removing a book from the member's borrowedBooksList
     @Test
-    void addBook() {
+    void addAndRemoveBook() {
         List<Book> borrowedList = member.addBorrowedBook(book);
         assertEquals(1, borrowedList.size());
         assertEquals(book, borrowedList.get(0));
     }
 
-    // Testing
+    // Testing removing a book from a member's borrowedBookList
+    @Test
+    void removeBook() {
+        List<Book> borrowedList = member.addBorrowedBook(book);
+        assertEquals(1, borrowedList.size());
+        member.removeBorrowedBook(book);
+        assertEquals(0, borrowedList.size());
+    }
+
+    // Testing updateMemberInfo
+    
 }
