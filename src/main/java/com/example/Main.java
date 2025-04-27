58655562a9b1f953029b4c92297c310e0a91ea72
Oreboat <tharper04@gmail.com>
2025-04-27 18:29:14 -0500

    rewrote Interface.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,10 +4,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Main method so manual testing can be done
-        Library library = new Library(); //Create a library
-        Librarians librarians = new Librarians();
-        InterfaceRedone ui = new InterfaceRedone();
-        ui.doInterface(library, librarians);
+        // Main method for manual testing
+        //Library library = new Library();  // Create a library
+        //Librarians librarians = new Librarians();
+
+        // Update: Instantiate and call the new Interface class
+        //Interface ui = new Interface();
+        Interface.loadInterface();
+        //ui.loadInterface(library, librarians);  // Start the new interface with the library and librarians
     }
 }
