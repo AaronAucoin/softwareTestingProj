12668a30983b67ad7665ba3bdcbaa5eee995c3ea
Steven Reed <stevenreed122@gmail.com>
2025-04-27 22:46:08 -0500

    set up testing

--- src/test/java/com/example/InterfaceTest.java
+++ src/test/java/com/example/InterfaceTest.java
@@ -1,6 +1,18 @@
 package com.example;
 
-public class InterfaceTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class InterfaceTest {
+    
     // testing options 9-12 below
+
 }
